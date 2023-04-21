0718c06779180bd170db9f92f614534c0cc8c15e
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-21 08:36:07 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/hibernateandjpaspringbootbasicapp/HibernateAndJpaSpringBootBasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.hibernateandjpaspringbootbasicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAndJpaSpringBootBasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAndJpaSpringBootBasicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/hibernateandjpaspringbootbasicapp/HibernateAndJpaSpringBootBasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.hibernateandjpaspringbootbasicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAndJpaSpringBootBasicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
