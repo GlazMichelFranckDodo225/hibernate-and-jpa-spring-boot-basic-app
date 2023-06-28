39dc178884ead560f1568fc1d30813e42cfd079d
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-28 23:15:32 +0200

    Addition of the "lombok" dependency to the "pom.xml" file and creation of the "User" entity

---
+++ src/main/java/com/dgmf/hibernateandjpaspringbootbasicapp/entity/User.java
@@ -0,0 +1,20 @@
+package com.dgmf.hibernateandjpaspringbootbasicapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+}
